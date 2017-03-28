9bd80a80fae3d7ff706cd7fe86fcfcd74af7454d
QqlimaaxX <mohitkarangiya@gmail.com>
2017-03-28 11:40:37 +0530

    added AI , and mana bar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app = new Vue({
         givenup: true,
         playerHealth: 100,
         enemyHealth: 100,
+        magic:100,
         won:false,
         lost:false,
         playerStyle: {
@@ -14,6 +15,10 @@ app = new Vue({
             width: '100%',
             transitionDuration: "300ms"
         },
+        magicStyle:{
+        	width:'100%',
+        	transitionDuration:"300ms"
+        },
         playerAttacks:[],
         enemyAttacks:[]
     },
@@ -21,6 +26,7 @@ app = new Vue({
         updateBars: function() {
             this.playerStyle.width = this.playerHealth + "%";
             this.enemyStyle.width = this.enemyHealth + "%";
+            this.magicStyle.width = this.magic+"%";
             // console.log(this.playerHealth,this.enemyHealth);
             if(this.enemyHealth<=0){
             	alert("You won!!");
@@ -43,9 +49,21 @@ app = new Vue({
             this.updateBars();
         },
         monsterAttack: function() {
-        	var atk = Math.floor(Math.random()*5+10);
-            this.playerHealth -= atk;
-            this.enemyAttacks.push(atk);
+        	if(this.enemyHealth<50 && Math.random()<0.5){
+        		//try to heal by 25% chance
+        			var hea = Math.floor(Math.random()*10+10);
+        			this.enemyHealth+=hea;
+        			this.enemyAttacks.push(-hea);
+        	}else{
+        		console.log(">50");
+	        	var atk = Math.floor(Math.random()*5+10);
+	            this.playerHealth -= atk;
+	            this.enemyAttacks.push(atk);
+	        }
+	        this.magic+=10;
+	        if(this.magic>100){
+	        	this.magic=100;
+	        }
             this.updateBars();
         },
 
@@ -56,20 +74,26 @@ app = new Vue({
             this.monsterAttack();
         },
         specialAttack: function() {
-        	var atk =Math.floor(Math.random()*12+8);
-            this.enemyHealth -= atk;
-            this.playerAttacks.push(atk);
-            this.monsterAttack();
+        	if(this.magic>=25){
+	        	var atk =Math.floor(Math.random()*12+8);
+	            this.enemyHealth -= atk;
+	            this.playerAttacks.push(atk);
+	            this.magic-=25;
+	            this.monsterAttack();
+            }
         },
         heal: function() {
-        	var hea=Math.floor(Math.random()*10+10);
-            this.playerHealth += hea;
-            this.playerAttacks.push(-hea);
-            this.monsterAttack();
-            if(this.playerHealth>100){
-            	this.playerHealth=100;
-            }
-            this.updateBars();
+        	if(this.magic>=30 && this.playerHealth<100){
+	        	var hea=Math.floor(Math.random()*10+10);
+	        	this.magic-=30;
+	            this.playerHealth += hea;
+	            this.playerAttacks.push(-hea);
+	            this.monsterAttack();
+	            if(this.playerHealth>100){
+	            	this.playerHealth=100;
+	            }
+	        }
+	            this.updateBars();
         },
         reverse: function (array) {
 			return array.slice().reverse()
